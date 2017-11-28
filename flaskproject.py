64f970dfe0bc65bcae557270523193da3f1a75ea
Steven Woo <swoo@tackable.com>
2017-11-27 19:42:52 -0800

    add resume

--- flaskproject.py
+++ flaskproject.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def hello_world():
     return "<h1 style='color:blue'>Welcome to swoo.club!</h1>"
 
 @app.route("/test")
@@ -13,6 +14,15 @@ def moretest():
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     return 'Post %d' % post_id
+
+@app.route('/hello/')
+@app.route('/hello/<input_name>')
+def hello(input_name=None):
+    return render_template('hello.html', name=input_name)
+
+@app.route('/resume/')
+def resume():
+    return render_template('resume.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
