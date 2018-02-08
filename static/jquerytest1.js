919ba810d3f9083ffb64ac3e9d7a80f4d3681f72
Steven Woo <stevenwoo@gmail.com>
2018-02-07 22:00:11 -0800

    step

--- static/jquerytest1.js
+++ static/jquerytest1.js
@@ -4,11 +4,32 @@ var notes = [
   {id: 3, body: "This is a third test", timestamp: Date.now()}
 ];
 
-notes.forEach(function(note) {
+function transformNotes(notes) {
+  return notes.slice().sort(function(a, b) {
+    return b.timestamp - a.timestamp;
+  });
+}
+
+function formatTitle(body) {
+  var maxLength = 20;
+  if (body.length > maxLength) {
+    return body.substring(0, maxLength - 3) + '...';
+  } else if (body.length === 0) {
+    return "New note";
+  } else {
+    return body;
+  }
+}
+
+function formatTimestamp(timestamp) {
+  return new Date(timestamp).toUTCString();
+}
+
+transformNotes(notes).forEach(function(note) {
   $('.note-selectors').append(
     '<div class="note-selector">' +
-      '<p class="note-selector-title">' + note.body + '</p>' +
-      '<p class="note-selector-timestamp">' + note.timestamp + '</p>' +
+          '<p class="note-selector-title">' + formatTitle(note.body) + '</p>' +
+          '<p class="note-selector-timestamp">' + formatTimestamp(note.timestamp) + '</p>' +
     '</div>'
   );
 });
