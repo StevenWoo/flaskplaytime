a5f7d3009103e64ada04ad02839e7d580798416e
Steven Woo <stevenwoo@gmail.com>
2018-02-11 23:36:24 -0800

    jquery fun

--- static/jquerytest1.js
+++ static/jquerytest1.js
@@ -1,11 +1,3 @@
-
-var notes = [
-  {id: 1, body: "This is a first test", timestamp: Date.now()},
-  {id: 2, body: "This is a second test", timestamp: Date.now()},
-  {id: 3, body: "This is a third test", timestamp: Date.now()}
-];
-
-
 function transformNotes(notes) {
   return notes.slice().sort(function(a, b) {
     return b.timestamp - a.timestamp;
@@ -27,14 +19,22 @@ function formatTimestamp(timestamp) {
   return new Date(timestamp).toUTCString();
 }
 
+function domCreateNoteSelector(note, selectedNote) {
+  var $noteSelector = $(
+    '<div class="note-selector' + (note === selectedNote ? ' active' : '') + '">' +
+      '<p class="note-selector-title">' + formatTitle(note.body) + '</p>' +
+      '<p class="note-selector-timestamp">' + formatTimestamp(note.timestamp) + '</p>' +
+    '</div>'
+  );
+  $noteSelector.data(note);
+  return $noteSelector;
+}
+
+
+
 function domCreateNoteSelectors(notes, selectedNote) {
   transformNotes(notes).forEach(function(note) {
-    var $noteSelector = $(
-      '<div class="note-selector' + (note === selectedNote ? ' active' : '') + '">' +
-        '<p class="note-selector-title">' + formatTitle(note.body) + '</p>' +
-        '<p class="note-selector-timestamp">' + formatTimestamp(note.timestamp) + '</p>' +
-      '</div>'
-    );
+    var $noteSelector = domCreateNoteSelector(note, selectedNote);
     $noteSelector.data(note);
     $('.note-selectors').append($noteSelector);
   });
@@ -57,19 +57,37 @@ $('.note-editor-input').on('input propertychange', function(event) {
   var timestamp = Date.now();
   $('.note-selector.active').data('body', body);
   $('.note-selector.active').data('timestamp', timestamp);
-
   // Update the DOM note editor timestamp
   $('.note-editor-info').html(formatTimestamp(timestamp));
-
   // Update the DOM currently selected note selector
   $('.note-selector.active .note-selector-title')
     .html(formatTitle(body));
   $('.note-selector.active .note-selector-timestamp')
     .html(formatTimestamp(timestamp));
-
   // Update the DOM note selectors sorting order
   var $active = $('.note-selector.active').detach();
   $('.note-selectors').prepend($active);
+});
+
+
+
+$('.toolbar-button-new').on('click', function() {
+  $('.note-selector').removeClass('active');
+  var note = {
+    id: Date.now(),
+    body: "",
+    timestamp: Date.now()
+  };
+  var $noteSelector = domCreateNoteSelector(note, note);
+  $('.note-selectors').prepend($noteSelector);
+  domUpdateNoteEditor(note);
+});
+
+
+
+$('.toolbar-button-delete').on('click', function() {
+    $('.note-selector.active').remove();
+    console.log('hello delete');
 });
 
 var notes = [
@@ -80,6 +98,3 @@ var notes = [
 var selectedNote = notes[0];
 domCreateNoteSelectors(notes, selectedNote);
 domUpdateNoteEditor(selectedNote);
-
-
-
